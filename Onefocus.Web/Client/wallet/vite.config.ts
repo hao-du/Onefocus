b0d71707f7387e8453546d0158db8d2ea0218bb8
hao-du <kevindu1986@outlook.com>
2025-03-09 13:53:30 +0700

    Config vite to integrate to Asp.Net core
    Config vite to integrate to Asp.Net core

--- Onefocus.Web/Client/wallet/vite.config.ts
+++ Onefocus.Web/Client/wallet/vite.config.ts
@@ -1,4 +1,5 @@
 import {defineConfig} from 'vite';
+import { JSDOM } from 'jsdom';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
@@ -6,23 +7,29 @@ export default defineConfig(({ mode }) => {
         plugins: [
             react(),
             {
-                name: "html-update-script-tag",
+                name: "html-remove-first-slash",
                 enforce: "post",
                 transformIndexHtml(html: string) {
-                    const regex = /<script type="module" crossorigin src="\//g;
-                    const replacement = '<script type="module" crossorigin src="';
-                    const output = html.replace(regex, replacement);
-                    return output;
-                },
-            },
-            {
-                name: "html-update-link-tag",
-                enforce: "post",
-                transformIndexHtml(html: string) {
-                    const regex = /<link rel="stylesheet" crossorigin href="\//g;
-                    const replacement = '<link rel="stylesheet" crossorigin href="';
-                    const output = html.replace(regex, replacement);
-                    return output;
+                    const jsdom = new JSDOM(html);
+                    console.log('\x1b[32m', '\nReplacing first "/" in <script> and <link> tags:');
+                    console.log('\x1b[33m')
+                    for (const script of jsdom.window.document.querySelectorAll('script')) {
+                        if(script.type == 'module' && script.src.startsWith('/')) {
+                            console.log('Replacing script with url: ' + script.src + '...');
+                            script.src = script.src.slice(1);
+                            console.log('Replaced to: ' + script.src);
+                        }
+                    }
+                    console.log('\x1b[34m')
+                    for (const link of jsdom.window.document.querySelectorAll('link')) {
+                        if(link.rel == 'stylesheet' && link.href.startsWith('/')) {
+                            console.log('Replacing link with url: ' + link.href + '...');
+                            link.href = link.href.slice(1);
+                            console.log('Replaced to: ' + link.href);
+                        }
+                    }
+                    console.log('\x1b[0m');
+                    return jsdom.serialize();
                 },
             },
         ],
--- Onefocus.Web/Program.cs
+++ Onefocus.Web/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,15 +14,23 @@ if (!app.Environment.IsDevelopment())
     
 }
 
-app.UseDefaultFiles();
+app.UseDefaultFiles(new DefaultFilesOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "Client/wallet/dist")),
+    RequestPath = "/wallet"
+});
 app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "Client/wallet/dist")),
     RequestPath = "/wallet"
 });
-
 app.UseRouting();
 
-app.MapFallbackToFile("/index.html"); 
+app.MapGet("/wallet", () =>
+{
+    return Results.Redirect("/wallet/index.html");
+});
+
+app.MapFallbackToFile("/wallet/index.html"); 
 
 app.Run();
